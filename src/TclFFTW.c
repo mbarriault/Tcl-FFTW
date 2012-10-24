8b7443b9a223198ec6af3ab47a03f11979216ab8
Michael Barriault <mikebarriault@me.com>
2012-10-24 10:24:23 -0230

    Added pkgIndex.tcl. Module compiles properly.

--- src/TclFFTW.c
+++ src/TclFFTW.c
@@ -1,9 +1,12 @@
+#include <stdlib.h>
 #include <tcl.h>
 #include <complex.h>
 #include <fftw3.h>
 
 int fft(ClientData cdata, Tcl_Interp* interp, int objc, struct Tcl_Obj* const objv[]) {
-    Tcl_Obj* list = Tcl_GetVar2Ex(interp, objv[1], NULL, TCL_LEAVE_ERR_MSG);
+    int len;
+    const char* listStr = Tcl_GetStringFromObj(objv[1], &len);
+    Tcl_Obj* list = Tcl_GetVar2Ex(interp, listStr, (char*)NULL, TCL_LEAVE_ERR_MSG);
     int count;
     Tcl_Obj** items;
     Tcl_ListObjGetElements(interp, list, &count, &items);
@@ -12,8 +15,8 @@ int fft(ClientData cdata, Tcl_Interp* interp, int objc, struct Tcl_Obj* const ob
     double* data = fftw_malloc(sizeof(data) * count);
     fftw_complex* out = fftw_malloc(sizeof(fftw_complex) * newCount);
     
-    fftw_plan plan = fftw_plan_dft_r2c_ld(count, data, out, FFTW_ESTIMATE);
-    fftw_execute(p);
+    fftw_plan plan = fftw_plan_dft_r2c_1d(count, data, out, FFTW_ESTIMATE);
+    fftw_execute(plan);
     
     Tcl_Obj** newItems = malloc(sizeof(Tcl_Obj*) * newCount);
     Tcl_Obj** newItem = malloc(sizeof(Tcl_Obj*) * 2);
@@ -23,11 +26,11 @@ int fft(ClientData cdata, Tcl_Interp* interp, int objc, struct Tcl_Obj* const ob
         newItems[i] = Tcl_NewListObj(2, newItem);
     }
     list = Tcl_NewListObj(newCount, newItems);
-    Tcl_SetListObj(Tcl_GetObjResult(interp), list);
+    Tcl_SetObjResult(interp, list);
     free(newItems);
     free(newItem);
     
-    fftw_destroy_plan(p);
+    fftw_destroy_plan(plan);
     fftw_free(data);
     fftw_free(out);
 }
---
+++ src/pkgIndex.tcl
@@ -0,0 +1,16 @@
+proc platform {} {
+    global tcl_platform
+    set plat [lindex $tcl_platform(os) 0]
+    return "$plat"
+}
+
+switch [platform] {
+    Linux {
+        set ext so
+    }
+    Darwin {
+        set ext dylib
+    }
+}
+
+package ifneeded fftw 0.5.0 [list load [file join $dir libTclFFTW.$ext] fftw]
